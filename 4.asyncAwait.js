15de21a0327408a3b90e8b49c84a49c70ddd4d2c
pkh7761 <park776139>
2019-03-19 10:31:10 +0900

    add async await

--- 4.asyncAwait.js
+++ 4.asyncAwait.js
@@ -8,12 +8,14 @@ const readdirAsync = util.promisify(fs.readdir);
 const path = './data';
 
 (async () => {
-   const files = await readdirAsync(path);
+    const files = await readdirAsync(path);
+    const filesPromises = files.map((file) => {
+        return readFileAsync(`${path}/${file}`, 'utf-8');
+    });
 
-   for(const file of files) {
-       const data = await readFileAsync(`${path}/${file}`, 'utf-8');
-
-       const {text} = JSON.parse(data);
-       console.log(text);
-   }
+    const resultArr = await Promise.all(filesPromises);
+    resultArr.forEach((data)=>{
+        const {text} = JSON.parse(data);
+        console.log(text);
+    })
 })();
